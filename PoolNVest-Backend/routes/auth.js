a3bb69183cfff782a60f0a0e0ef11ee06116c546
Admin <manethkulatunge@gmail.com>
2022-03-14 04:25:24 -0400

    used general bycrypt related sign up route

--- PoolNVest-Backend/routes/auth.js
+++ PoolNVest-Backend/routes/auth.js
@@ -6,6 +6,20 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+  // signup route
+  router.post("/signup", async (req, res) => {
+    const body = req.body;
+
+    if (!(body.email && body.password)) {
+      return res.status(400).send({ error: "Data not formatted properly" });
+    }
+
+    const user = new User(body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save().then((doc) => res.status(201).send(doc));
+  });
+
 router.post('/login', async (req,res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
